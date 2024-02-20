f335b2f9050e84db53db234d4ae520b070f9da67
kodxana <romanrex19@int.pl>
2024-02-20 22:10:47 +0100

    Update to version 1.1

--- scripts/installer.sh
+++ scripts/installer.sh
@@ -3,12 +3,16 @@
 # Define where to save the binaries
 TUSD_BIN_PATH="/usr/local/bin/tusd"
 RUNPOD_UPLOADER_BIN_PATH="/usr/local/bin/runpod-uploader"
+RUNPOD_UPLOADER_SCRIPT_PATH="/etc/runpod-uploader/scripts"
 
 # Define the directory for hooks
 HOOKS_DIR="/etc/tusd/hooks"
 
 # Create the hooks directory
 mkdir -p "${HOOKS_DIR}"
+
+# Create the scripts directory
+mkdir -p "${RUNPOD_UPLOADER_SCRIPT_PATH}"
 
 # Download and setup tusd
 echo "Downloading and setting up tusd..."
@@ -19,7 +23,7 @@ chmod +x "${TUSD_BIN_PATH}"
 
 # Download and setup runpod-uploader binary
 echo "Downloading and setting up runpod-uploader binary..."
-curl -L https://github.com/kodxana/RunPod-FilleUploader/releases/download/v1.0/runpod-uploader -o "${RUNPOD_UPLOADER_BIN_PATH}"
+curl -L https://github.com/kodxana/RunPod-FilleUploader/releases/download/v1.1/runpod-uploader -o "${RUNPOD_UPLOADER_BIN_PATH}"
 chmod +x "${RUNPOD_UPLOADER_BIN_PATH}"
 
 # Download and setup hook scripts
@@ -28,6 +32,10 @@ curl -L https://github.com/kodxana/RunPod-FilleUploader/raw/main/hook/post-finis
 curl -L https://github.com/kodxana/RunPod-FilleUploader/raw/main/hook/rename_uploaded_file.py -o "${HOOKS_DIR}/rename_uploaded_file.py"
 chmod +x "${HOOKS_DIR}/post-finish"
 chmod +x "${HOOKS_DIR}/rename_uploaded_file.py"
+curl -L https://github.com/kodxana/RunPod-FilleUploader/raw/main/scripts/ssh-setup.sh -o "${RUNPOD_UPLOADER_SCRIPT_PATH}/ssh-setup.sh"
+curl -L https://github.com/kodxana/RunPod-FilleUploader/raw/main/scripts/run-speedtest.sh -o "${RUNPOD_UPLOADER_SCRIPT_PATH}/run-speedtest.sh"
+chmod +x "${RUNPOD_UPLOADER_SCRIPT_PATH}/ssh-setup.sh"
+chmod +x "${RUNPOD_UPLOADER_SCRIPT_PATH}/run-speedtest.sh"
 
 # Clean up downloaded and extracted files
 rm -rf tusd.tar.gz tusd_linux_amd64
@@ -36,3 +44,4 @@ echo "Setup completed successfully."
 echo "tusd is now available at ${TUSD_BIN_PATH}"
 echo "runpod-uploader is now available at ${RUNPOD_UPLOADER_BIN_PATH}"
 echo "Hook scripts are set up at ${HOOKS_DIR}"
+echo "Aditional scripts saved to ${RUNPOD_UPLOADER_SCRIPT_PATH}"
